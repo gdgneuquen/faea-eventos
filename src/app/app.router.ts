795b1fbfb2f6506544845fcb26057996f2c31b4d
SandyVeliz <sandy.e.veliz@gmail.com>
2018-05-07 18:30:59 -0300

    Fix LazyRoute Webpack error

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthenticationGuard } from './shared/services/authentication.guard';
 import { LoginComponent } from './shared/components/login/login.component';
+import { CoreModule } from './core/core.module';
 
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: 'app/core/core.module#CoreModule',
+    loadChildren: () => CoreModule,
   }
 ];
 
