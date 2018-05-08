6cc52126d011f667035e3d2500a70bf7561f0bdf
SandyVeliz <sandy.e.veliz@gmail.com>
2018-05-07 21:47:55 -0300

    Agrega Color Estado con NgClass

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FirebasedbService } from '../services/firebasedb.service';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import * as moment from 'moment';
-import { Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 import { TimerObservable } from 'rxjs/observable/TimerObservable';
-import {FilterPipe} from './pipes'
+import { FilterPipe } from './pipes'
 
 
 export interface Evento {
@@ -33,13 +33,13 @@ export class HomeComponent implements OnInit {
   horaActual = moment().locale('es').format('LT');
   hoy = moment().locale('es').format('LLLL');//fecha
   dia = moment().locale('es').format('dddd');//dia de la semana
-  mediahora=1800000;//en milisegundos
-  diezminutos=600000;//en ms
-  unminuto=60000;//en ms
+  mediahora = 1800000;//en milisegundos
+  diezminutos = 600000;//en ms
+  unminuto = 60000;//en ms
 
   queryString: string;
 
-  private subscription : Subscription;
+  private subscription: Subscription;
 
   constructor(
     private fbDb: FirebasedbService,
@@ -49,32 +49,32 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     let timer = TimerObservable.create(this.diezminutos, this.diezminutos);
-    this.subscription=timer.subscribe(t=>{
+    this.subscription = timer.subscribe(t => {
       this.getEventos();
     })
     this.getEventos();
   }
 
-  getEventos(){
+  getEventos() {
     this.fbDb.getEvents().subscribe(events => {
       this.eventos = [];
       events.forEach(event => this.filterCurrentActivity(event))
-    }) 
+    })
   }
 
   filterCurrentActivity(evento: Evento) {
     // evento que esta en la semana actual, y que sea hoy
-    if ( this.belongsToPeriodo(evento)
-        &&  this.isEventValid(evento)) {
-          var evento2 = {
-            horario: evento.horaInicio + ' a ' + evento.horaFin,
-            actividad: evento.descripcion,
-            tipo: evento.tipoActividad,
-            profesor: evento.nombre,
-            aula: evento.zonaAula,
-            estado: evento.estadoActividad,
-            }
-        this.eventos.push(evento2);
+    if (this.belongsToPeriodo(evento)
+      && this.isEventValid(evento)) {
+      var evento2 = {
+        horario: evento.horaInicio + ' a ' + evento.horaFin,
+        actividad: evento.descripcion,
+        tipo: evento.tipoActividad,
+        profesor: evento.nombre,
+        aula: evento.zonaAula,
+        estado: evento.estadoActividad,
+      }
+      this.eventos.push(evento2);
     }
   }
   //que la fecha de hoy este en el periodo de la Actividad
@@ -85,13 +85,30 @@ export class HomeComponent implements OnInit {
   belongsToToday(actividad: Evento) {
     const currentFromDayFrom = moment(actividad.pickerDesde).locale('es');
     const currentFromDayTo = moment(actividad.pickerHasta).locale('es');
-    return moment().locale('es').diff(currentFromDayFrom ,'days') === 0 || moment().locale('es').diff(currentFromDayTo ,'days') === 0;
+    return moment().locale('es').diff(currentFromDayFrom, 'days') === 0 || moment().locale('es').diff(currentFromDayTo, 'days') === 0;
   }
   // Controlar el dia seleccionado en array y la hora vencida
   isEventValid(actividad: Evento) {
     return (actividad.dias[moment().locale('es').weekday()]
-         // && moment().locale('es').format('HH:mm') >= actividad.horaInicio);
-          && moment().locale('es').format('HH:mm') <= actividad.horaFin);
+      // && moment().locale('es').format('HH:mm') >= actividad.horaInicio);
+      && moment().locale('es').format('HH:mm') <= actividad.horaFin);
   }
-
+  setStyle(estado) {
+    switch (estado) {
+      case "Normal": {
+        let style = {
+          'color': estado = "Normal" ? 'green' : 'black'
+        }
+        return style
+        break;
+      }
+      case "Cancelado": {
+        let style = {
+          'color': estado = "Cancelado" ? 'red' : 'black'
+        }
+        return style
+        break;
+      }
+    }
+  }
 }
